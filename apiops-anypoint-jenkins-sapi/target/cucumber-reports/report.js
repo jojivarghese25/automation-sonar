dc9b384cb1c81742c43f82ead6512d4dd19e2a5d
Joji Varghese <jojihr@gmail.com>
2020-11-09 18:16:47 +0000

    initial commit

---
+++ apiops-anypoint-jenkins-sapi/target/cucumber-reports/report.js
@@ -0,0 +1,223 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("customer.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Testing the API\u0027s",
+  "description": "",
+  "id": "testing-the-api\u0027s",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Get Subscribed customer details",
+  "description": "",
+  "id": "testing-the-api\u0027s;get-subscribed-customer-details",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I create a new request with http://apiops-bdd-sapi.us-e2.cloudhub.io/api/ service",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I add the users/registerUser/subscription endpoint to the service",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 6,
+  "name": "I send the GET request to the service",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I get the 200 response code",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://apiops-bdd-sapi.us-e2.cloudhub.io/api/",
+      "offset": 28
+    }
+  ],
+  "location": "CustomerStepDefinition.createNewRequest(String)"
+});
+formatter.result({
+  "duration": 74145200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "users/registerUser/subscription",
+      "offset": 10
+    }
+  ],
+  "location": "CustomerStepDefinition.addEndpoint(String)"
+});
+formatter.result({
+  "duration": 58900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "GET",
+      "offset": 11
+    }
+  ],
+  "location": "CustomerStepDefinition.sendRequest(String)"
+});
+formatter.result({
+  "duration": 3349827400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 10
+    }
+  ],
+  "location": "CustomerStepDefinition.responseCodeValidation(int)"
+});
+formatter.result({
+  "duration": 26857500,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "Register Customer",
+  "description": "",
+  "id": "testing-the-api\u0027s;register-customer",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 11,
+  "name": "I create a new request with http://apiops-bdd-sapi.us-e2.cloudhub.io/api/ service",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "I add the users/registerUser endpoint to the service",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I pass application/json as content type",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "I send the values of src/test/resources/cucumberResources/registerCustomerInput.json in the request body",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "I send the POST request to the service",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "I get the 201 response code",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 17,
+  "name": "I expect the values of src/test/resources/cucumberResources/registerCustomerOutput.json in the response body",
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://apiops-bdd-sapi.us-e2.cloudhub.io/api/",
+      "offset": 28
+    }
+  ],
+  "location": "CustomerStepDefinition.createNewRequest(String)"
+});
+formatter.result({
+  "duration": 32300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "users/registerUser",
+      "offset": 10
+    }
+  ],
+  "location": "CustomerStepDefinition.addEndpoint(String)"
+});
+formatter.result({
+  "duration": 26200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "application/json",
+      "offset": 7
+    }
+  ],
+  "location": "CustomerStepDefinition.setContentType(String)"
+});
+formatter.result({
+  "duration": 27600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "src/test/resources/cucumberResources/registerCustomerInput.json",
+      "offset": 21
+    }
+  ],
+  "location": "CustomerStepDefinition.setRequestBody(String)"
+});
+formatter.result({
+  "duration": 146517400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "POST",
+      "offset": 11
+    }
+  ],
+  "location": "CustomerStepDefinition.sendRequest(String)"
+});
+formatter.result({
+  "duration": 1567713900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "201",
+      "offset": 10
+    }
+  ],
+  "location": "CustomerStepDefinition.responseCodeValidation(int)"
+});
+formatter.result({
+  "duration": 1362500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "src/test/resources/cucumberResources/registerCustomerOutput.json",
+      "offset": 23
+    }
+  ],
+  "location": "CustomerStepDefinition.responseAttributeValidation(String)"
+});
+formatter.result({
+  "duration": 403657300,
+  "status": "passed"
+});
+});
